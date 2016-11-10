04e7c010b82beb88b2fbe753a43bf382bec0cf88
László Székely-Tóth <notisracer@gmail.com>
2016-11-10 18:30:21 +0100

    Step 3

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 
-def display_inventory(inventory):
-    """ Step 1 """
+# helpers
+
+def count_values(inventory):
+    """ Returns sum of a dictionary's values """
     itemCount = 0
-    print("Inventory:")
     for item in inventory:
         itemCount += inventory[item]
+    return itemCount
+
+
+def calculate_padding(items):
+    """ Returns the length of the longest item in a list """
+    maxLength = 0
+    for item in items:
+        length = len(str(item))
+        if length > maxLength:
+            maxLength = length
+    return maxLength
+
+
+# main task
+
+def display_inventory(inventory):
+    """ Step 1 """
+    itemCount = count_values(inventory)
+    print("Inventory:")
+    for item in inventory:
         print(inventory[item], item)
     print("Total number of items:", itemCount)
 
@@ -20,6 +41,38 @@ def add_to_inventory(inventory, added_items):
             inventory[item] = 1
     return inventory
 
+
+def print_table(order):
+    """ Step 3 """
+# Calculating padding
+    # min 5 because the length of 'count'
+    maxCountLength = max(5, calculate_padding(inv.values()))
+    # min 9 because the length of 'item name'
+    maxNameLength = max(9, calculate_padding(inv))
+# Printing header
+    print("Inventory:")
+    print("  " + "count".rjust(maxCountLength) + "  " +
+          "  " + "item name".rjust(maxNameLength))
+    print("-" * (2 + maxCountLength + 2 +
+                 2 + maxNameLength))
+# Sorting body
+    if order == "count,desc":
+        sortedKeys = sorted(inv, key=inv.__getitem__, reverse=True)
+    elif order == "count,asc":
+        sortedKeys = sorted(inv, key=inv.__getitem__, reverse=False)
+    else:
+        sortedKeys = inv.keys()
+# Printing body
+    for key in sortedKeys:
+        print("  " + str(inv[key]).rjust(maxCountLength) + "  " +
+              "  " + str(key).rjust(maxNameLength))
+# Printing footer
+    print("-" * (2 + maxCountLength + 2 +
+                 2 + maxNameLength))
+    print("Total number of items:", count_values(inv))
+
+
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inv = add_to_inventory(inv, dragon_loot)
-display_inventory(inv)
+# display_inventory(inv)
+print_table("count,desc")
