24548baefe9bf46a9c39e039c19d60db31862c38
László Székely-Tóth <notisracer@gmail.com>
2016-11-10 16:56:43 +0100

    Step 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 
 def display_inventory(inventory):
+    """ Step 1 """
     itemCount = 0
     print("Inventory:")
     for item in inventory:
@@ -9,4 +10,16 @@ def display_inventory(inventory):
         print(inventory[item], item)
     print("Total number of items:", itemCount)
 
+
+def add_to_inventory(inventory, added_items):
+    """ Step 2 """
+    for item in added_items:
+        if item in inventory.keys():
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+    return inventory
+
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = add_to_inventory(inv, dragon_loot)
 display_inventory(inv)
